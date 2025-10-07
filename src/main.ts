0be3a1b3f8f56bd903fda4460c1ff305d5694a65
mlau16 <mlau16@ucsc.edu>
2025-10-07 19:20:37 +0000

    Added cat button (Step 1 done)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
+import exampleIconUrl from "./cat.png";
 import "./style.css";
 
 document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+  <button> <p><img src="${exampleIconUrl}" class="icon" /></p>
+  </button>
 `;
