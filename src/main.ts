0a0ccf79b73c7f8f8a302f1bfab255dbb6c21393
mlau16 <mlau16@ucsc.edu>
2025-10-07 19:34:50 +0000

    Added cat counter (Step 2 complete)

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,20 @@
 import exampleIconUrl from "./cat.png";
 import "./style.css";
 
+//counter
+let counter: number = 0;
+
 document.body.innerHTML = `
-  <button> <p><img src="${exampleIconUrl}" class="icon" /></p>
+  <p>Cats: <span id="counter">0</span></p>
+  <button id="increment"> <p><img src="${exampleIconUrl}" class="icon" /></p>
   </button>
 `;
+
+//Add click handler
+const button = document.getElementById("increment")!;
+const counterElement = document.getElementById("counter")!
+
+button.addEventListener("click", () => {
+  counter += 1;
+  counterElement.textContent = counter.toString();
+})
