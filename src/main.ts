41e4cc9b822fa9c8457e1cc7177508afaccf692a
mlau16 <mlau16@ucsc.edu>
2025-10-07 21:43:35 +0000

    Added a milk upgrade - costs 10 cats - increases growth rate by 1 - can only buy when you have 10 units
    (Step 5 completed)

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,22 @@
-import exampleIconUrl from "./cat.png";
+import catIconUrl from "./cat.png";
+import milkIconUrl from "./milk.png";
 import "./style.css";
 
 //counter
 let counter: number = 0;
+let growthRate: number = 0;
 
 document.body.innerHTML = `
   <p>Cats: <span id="counter">0</span></p>
-  <button id="increment"> <p><img src="${exampleIconUrl}" class="icon" /></p>
+  <button id="increment"><img src="${catIconUrl}" class="icon" />
   </button>
+  <header> Items: </header>
+  <button id="buyMilk" disabled> <img src="${milkIconUrl}" class="icon" /> </button>
 `;
 
 //Add click handler
 const button = document.getElementById("increment")!;
+const milkButton = document.getElementById("buyMilk")! as HTMLButtonElement;
 const counterElement = document.getElementById("counter")!;
 
 button.addEventListener("click", () => {
@@ -19,9 +24,18 @@ button.addEventListener("click", () => {
   updateDisplay();
 });
 
+milkButton.addEventListener("click", () => {
+  counter -= 10;
+  growthRate += 1;
+  updateDisplay();
+});
+
 //Update Counter Display
 function updateDisplay() {
-  counterElement.textContent = Math.floor(counter).toString();
+  const intCounter = Math.floor(counter);
+  counterElement.textContent = intCounter.toString();
+
+  milkButton.disabled = intCounter < 10;
 }
 
 //Animation using requestAnimationFrame
@@ -30,7 +44,7 @@ let lastTime: number | null = null;
 function animate(currentTime: number) {
   if (lastTime !== null) {
     const deltaTime = (currentTime - lastTime) / 1000;
-    counter += deltaTime;
+    counter += deltaTime * growthRate;
     updateDisplay();
   }
   lastTime = currentTime;
